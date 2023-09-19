e75c7e67b5182c23f6de2ec7daa30f933ea38cd4
Rick Ricarte <ricarterick@outlook.com>
2023-09-19 01:14:34 -0300

    ML - metricas e matriz confusao no terminal

--- classificadores.py
+++ classificadores.py
@@ -3,13 +3,13 @@ from sklearn.calibration import cross_val_predict, LinearSVC
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer, TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 from sklearn import datasets, linear_model
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 
 #region Validacao cruzada
-def execute_model_multinomial(x_train, y_train):
+def execute_naive_bayes(x_train, y_train):
     nb_classifier = MultinomialNB()
     nb_classifier.fit(x_train, y_train)
 
@@ -25,13 +25,13 @@ def execute_logistic_regression(x_train, y_train):
 
     return results_lr
 
-def execute_SVC(x_train, y_train):
-    svc_classifier = LinearSVC()
-    svc_classifier.fit(x_train, y_train)
+def execute_SVM(x_train, y_train):
+    svm_classifier = LinearSVC()
+    svm_classifier.fit(x_train, y_train)
 
-    results_svc = cross_val_predict(svc_classifier, x_train, y_train, cv = 10)
+    results_svm = cross_val_predict(svm_classifier, x_train, y_train, cv = 10)
 
-    return results_svc
+    return results_svm
 
 def execute_random_forest(x_train, y_train):
     rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
@@ -67,19 +67,60 @@ def train(filePath, test_size):
     x_train_tfidf = tfidf_transformer.fit_transform(word_count_matrix)
 
     # Executando validação cruzada
-    results_nb = execute_model_multinomial(x_train_tfidf, y_train)
+    results_nb = execute_naive_bayes(x_train_tfidf, y_train)
 
     results_lr = execute_logistic_regression(x_train_tfidf, y_train)
 
-    results_svc = execute_SVC(x_train_tfidf, y_train)
+    results_svm = execute_SVM(x_train_tfidf, y_train)
 
     results_rf = execute_random_forest(x_train_tfidf, y_train)
 
-    print('results_nb')
-    print(results_nb)
-    print('results_lr')
-    print(results_lr)
-    print('results_svc')
-    print(results_svc)
-    print('results_rf')
-    print(results_rf)
+    # Coletando metricas [acuracia, precisao, revocacao]
+    metrics_nb = [accuracy_score(y_train, results_nb),
+                  precision_score(y_train, results_nb), 
+                  recall_score(y_train, results_nb)]
+    
+    metrics_lr = [accuracy_score(y_train, results_lr),
+                    precision_score(y_train, results_lr),
+                    recall_score(y_train, results_lr)]
+    
+
+    metrics_svm = [accuracy_score(y_train, results_svm),
+                    precision_score(y_train, results_svm),
+                    recall_score(y_train, results_svm)]
+    
+    metrics_rf = [accuracy_score(y_train, results_rf),
+                    precision_score(y_train, results_rf),
+                    recall_score(y_train, results_rf)]
+    
+    # Printando metricas
+    print('Naive Bayes')
+    print('Acuracia: ', metrics_nb[0])
+    print('Precisao: ', metrics_nb[1])
+    print('Revocacao: ', metrics_nb[2])
+
+    print('\nLogistic Regression')
+    print('Acuracia: ', metrics_lr[0])
+    print('Precisao: ', metrics_lr[1])
+    print('Revocacao: ', metrics_lr[2])
+
+    print('\nSVM')
+    print('Acuracia: ', metrics_svm[0])
+    print('Precisao: ', metrics_svm[1])
+    print('Revocacao: ', metrics_svm[2])
+
+    print('\nRandom Forest')
+    print('Acuracia: ', metrics_rf[0])
+    print('Precisao: ', metrics_rf[1])
+    print('Revocacao: ', metrics_rf[2])
+
+    # Matriz de confusao
+    print('\nMatrizes confusao')
+    print('Naive Bayes')
+    print(pd.crosstab(y_train, results_nb, rownames=['Real'], colnames=['Predito'], margins=True))
+    print('\nLogistic Regression')
+    print(pd.crosstab(y_train, results_lr, rownames=['Real'], colnames=['Predito'], margins=True))
+    print('\nSVM')
+    print(pd.crosstab(y_train, results_svm, rownames=['Real'], colnames=['Predito'], margins=True))
+    print('\nRandom Forest')
+    print(pd.crosstab(y_train, results_rf, rownames=['Real'], colnames=['Predito'], margins=True))
